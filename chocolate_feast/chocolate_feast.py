4d0be1b7b0b87b2a3f16069b86d3682ac475ddb3
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2014-09-27 02:08:59 -0400

    Solved Chocolate Feast

---
+++ chocolate_feast/chocolate_feast.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# https://www.hackerrank.com/challenges/chocolate-feast
+
+import sys
+
+def num_chocolates(money, cost, exchange):
+    choco = money // cost
+    wrappers = choco
+    while wrappers > exchange:
+        exch_choco, wrappers = divmod(wrappers, exchange)
+        wrappers += exch_choco
+        choco += exch_choco
+    return choco
+
+def main():
+    T = int(sys.stdin.readline())
+    for _ in range(T):
+        [N, C, M] = [ int(x) for x in sys.stdin.readline().split() ]
+        print(num_chocolates(N, C, M))
+
+if __name__ == '__main__':
+    main()
