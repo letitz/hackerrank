e4fcc79687fff13cbf179271577790b8115acb78
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2014-09-27 00:49:34 -0400

    Solved Maximizing XOR

---
+++ max_xor/max_xor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# https://www.hackerrank.com/challenges/maximizing-xor
+
+import sys
+
+def max_xor(L,R):
+	res = 1
+	n = L ^ R
+	while n > 0:
+		n >>=1
+		res <<= 1
+	return res - 1
+
+def main():
+	L = int(sys.stdin.readline())
+	R = int(sys.stdin.readline())
+	print(max_xor(L,R))
+
+if __name__ == '__main__':
+	main()
