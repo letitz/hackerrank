f91cdbf18bb7e06d1c3cd069e0d46a123f5ced86
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2014-09-27 00:26:54 -0400

    Solved Love Letter

---
+++ love_letter/love_letter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+def char_distance(ca,cb):
+	return abs(ord(ca) - ord(cb))
+
+def palindrome_distance(str):
+	length = len(str)
+	dist = 0
+	for i in range(length // 2):
+		dist += char_distance(str[i], str[-i-1])
+	return dist
+
+def main():
+	T = int(sys.stdin.readline())
+	for _ in range(T):
+		print(palindrome_distance(sys.stdin.readline().strip()))
+
+main()
