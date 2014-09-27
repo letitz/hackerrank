26ce808c069ca514c9d5ea1ecab41d1772070ef0
Titouan Rigoudy <titouan.rigoudy@gmail.com>
2014-09-27 02:41:54 -0400

    Solved Manasa and Stones

---
+++ manasa_and_stones/manasa_and_stones.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# https://www.hackerrank.com/challenges/manasa-and-stones
+
+import sys
+
+def print_possible_values(num_stones, a, b):
+    if num_stones <= 1:
+        print(0)
+    if a == b:
+        print((num_stones - 1) * a)
+    if a > b:
+        tmp = a
+        a = b
+        b = tmp
+
+    mini = a * (num_stones - 1)
+    maxi = b * (num_stones - 1)
+    step = b - a
+    values = [ str(n) for n in range(mini, maxi + 1, step) ]
+    print(' '.join(values))
+
+def main():
+    num_tests = int(sys.stdin.readline())
+    for _ in range(num_tests):
+        num_stones = int(sys.stdin.readline())
+        a = int(sys.stdin.readline())
+        b = int(sys.stdin.readline())
+        print_possible_values(num_stones, a, b)
+
+if __name__ == '__main__':
+    main()
